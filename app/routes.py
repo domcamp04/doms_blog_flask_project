50b25cb1aaa916bb3c7a13ba15ddbe46fca2fb0e
Dominick Campanile <Dominick@Dominicks-MacBook-Air.local>
2021-10-13 16:46:43 -0500

    Second Commit

---
+++ app/routes.py
@@ -0,0 +1,16 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+def index():
+    name = 'Dominick'
+    title = 'Coding Temple Flask'
+    return render_template('index.html', name_of_user=name, title=title)
+
+
+@app.route('/products')
+def test():
+    title = 'Coding Temple Products'
+    products = ['apple', 'orange', 'banana', 'peach']
+    return render_template('products.html', title=title, products=products)
--- run.py
+++ run.py
@@ -1,16 +1,4 @@
-from flask import Flask
+from app import app
 
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return 'Hello World!'
-
-
-@app.route('/test')
-def test():
-    return 'test123'
 if __name__ == '__main__':
     app.run()
